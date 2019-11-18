9d2aa59818a81a3eade57bff69da95a728f475c2
dastansandso <patil8356@gmail.com>
2019-11-19 01:30:34 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { render } from 'react-dom';
 import Hello from './Hello';
+import AppBar from '@material-ui/core/AppBar';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import './style.css';
 
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'left',
+    },
+
+}));
+
+
 class App extends Component {
+  
   constructor() {
     super();
     this.state = {
@@ -12,12 +37,57 @@ class App extends Component {
   }
 
   render() {
+    
     return (
-      <div>
+      <div className={useStyles.root}>
+       <AppBar className=" headerbar "  position="fixed">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" className={useStyles.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton> 
+                    <div className={useStyles.title}>
+                        Header
+                    </div>
+                    <Button><a  color="primary" >Login</a></Button>
+                    <Button><a  color="primary" >Register</a></Button>
+                </Toolbar>
+            </AppBar>
+      <div className="card p-2 mt-5">
         <Hello name={this.state.name} />
         <p>
           Start editing to see some magic happen :)
         </p>
+        </div>
+            <div className="card p-2">
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+        </div>
+            <div className="card p-2">
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+        </div>
+            <div className="card p-2">
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+        </div>
+            <div className="card p-2">
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+        </div>
+            <div className="card p-2">
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+        </div>
       </div>
     );
   }
