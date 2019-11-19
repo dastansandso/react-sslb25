9ff7e73e1a162e44d80a1d282e684acc55056236
dastansandso <patil8356@gmail.com>
2019-11-20 02:43:20 +0530

    3 files modified

--- infocard/skillcard.js
+++ infocard/skillcard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import Card from'@material-ui/core/Card';
 import CardHeader from'@material-ui/core/CardHeader';
 import CardContent from'@material-ui/core/CardContent';
@@ -8,15 +9,19 @@ const Skillcard =(props)=>{
  
 return(
   <div className="container">
-   <div className="row no-gutters">
+   <div className="row justify-content-center">
   {props.skils[0].myskills.map(myskill => {
   return(
-    <div className="col text-center">
-    <Card className="card ">
-      <CardHeader 
-            title={myskill.skill} 
-            subheader={myskill.value} 
+    <div className="col-2 text-center">
+    <Card className="card skill-card my-2">
+    <CardContent className="pb-0">
+        <CircularProgressbar value={myskill.value} text={myskill.value} />
+        
+        </CardContent>
+      <CardHeader className="pt-0"
+            title={myskill.skill}
         />
+        
     </Card>
     </div>
   )
